3a678597ed8d38886a65f7a5b375f5e1b31ae47f
Abdullah Azmy <abdullahazmy2@gmail.com>
2025-06-30 05:20:27 +0300

    Add EmailConfiguration class and update namespace style
    Removed unused `using` directives from `EmailConfiguration.cs`.
    Updated `namespace` declaration to use curly braces `{ }`.
    Added `EmailConfiguration` class with `From` and `SmtpServer` properties,
    both initialized with non-null default values (`null!`).

--- TrackRoom.Services/DTOs/Email/EmailConfiguration.cs
+++ TrackRoom.Services/DTOs/Email/EmailConfiguration.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace BrainHope.Services.DTO.Email
+﻿namespace BrainHope.Services.DTO.Email
 {
     public class EmailConfiguration
     {
